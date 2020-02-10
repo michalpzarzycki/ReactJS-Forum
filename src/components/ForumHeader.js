1c509e7e84b8e8330e1477324b883c4c5e057805
Michal Zarzycki <michal.p.zarzycki@wp.pl>
2020-02-10 01:57:01 +0100

    Set the firebase firestore

--- src/components/ForumHeader.js
+++ src/components/ForumHeader.js
@@ -1,11 +1,31 @@
 import React, { useState } from 'react'
 import styles from './Header.module.css'
 import { Button, Icon, Header } from 'semantic-ui-react'
+import firebase from '../firebase/firebase'
+
 
 
 
 function ForumHeader() {
 const [isLoggedIn, setIsLoggedIn] = useState(false)
+
+function handleClick() {
+  let newLink = {
+    category:"ReactJS",
+    title:"Hooki",
+    description:"Hooki, komponenty fukncyjne, custom hooki - to wszytsko tutaj",
+    postsNumber:"",
+    lastPostCreated: {
+      name:"Użytkownik",
+      title:"Temat"
+    },
+    image:"https://via.placeholder.com/50"
+  
+  }
+  firebase.db.collection('first_cat').add(newLink);
+  console.log("UDALO SIE")
+
+}
     return(
         <div  className={styles.mainDiv}>
             <div className={styles.logoSection}>
@@ -17,7 +37,7 @@ const [isLoggedIn, setIsLoggedIn] = useState(false)
             <div className={styles.btnSection}>
          {isLoggedIn ?    <Button basic inverted color='blue'>
         Wyloguj
-      </Button> :     <Button basic inverted color='blue'>
+      </Button> :     <Button basic inverted color='blue' onClick={handleClick}>
         Zaloguj
       </Button>}
             </div>
---
+++ src/firebase/config.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyDTyEtauGfSz4RP4FcmncWoF9RUX8Tc0Kw",
+    authDomain: "forum-1e5c5.firebaseapp.com",
+    databaseURL: "https://forum-1e5c5.firebaseio.com",
+    projectId: "forum-1e5c5",
+    storageBucket: "forum-1e5c5.appspot.com",
+    messagingSenderId: "631879022901",
+    appId: "1:631879022901:web:40701181c9a36aeaf8747c"
+  };
+
+  export default firebaseConfig
